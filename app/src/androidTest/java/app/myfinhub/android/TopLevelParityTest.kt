43e037c0ac39167949716199471c4a3bccea7cfc
mariog <28479056+MariosGiannakaras@users.noreply.github.com>
2026-08-23 12:31:06 +0300

    test: fix Compose v2 node assertion import

--- app/src/androidTest/java/app/myfinhub/android/TopLevelParityTest.kt
+++ app/src/androidTest/java/app/myfinhub/android/TopLevelParityTest.kt
@@ -1,6 +1,5 @@
 package app.myfinhub.android
 
-import androidx.compose.ui.test.assertDoesNotExist
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.v2.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
