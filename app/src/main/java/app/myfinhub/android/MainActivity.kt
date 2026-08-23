9730fabdb363f71840b6c867648956ed934b7349
mariog <28479056+MariosGiannakaras@users.noreply.github.com>
2026-08-23 13:12:44 +0300

    feat: gate production launcher with auth root

--- app/src/main/java/app/myfinhub/android/MainActivity.kt
+++ app/src/main/java/app/myfinhub/android/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.fragment.app.FragmentActivity
-import app.myfinhub.android.app.MyFinHubApp
+import app.myfinhub.android.app.MyFinHubRoot
 
 class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyFinHubApp()
+            MyFinHubRoot()
         }
     }
 }
